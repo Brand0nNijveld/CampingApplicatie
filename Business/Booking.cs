85d9384dada5293989d5c2824afff61330265287
Vincent van Noord <49689172+VissentvNoord@users.noreply.github.com>
2024-11-18 12:07:06 +0100

    CampingAvailabilityService voor het zoeken van beschikbare plekken, en de basis klassen voor de campingplaatsen.

---
+++ Business/Booking.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampingApplication.Business
+{
+    public class Booking
+    {
+        public DateTime StartDate { get; private set; }
+        public DateTime EndDate { get; private set; }
+
+        public Booking(DateTime startDate, DateTime endDate)
+        {
+            this.StartDate = startDate;
+            this.EndDate = endDate;
+        }
+    }
+}
