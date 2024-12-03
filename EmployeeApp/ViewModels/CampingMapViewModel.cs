abf57a90c79c338068ae17f3c507dd53d7f65488
Brand0nNijveld <s1206311@student.windesheim.nl>
2024-12-03 10:51:44 +0100

    Start of UI

---
+++ EmployeeApp/ViewModels/CampingMapViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CampingApplication.Business;
+using CampingApplication.VisitorApp.Models;
+
+namespace CampingApplication.VisitorApp.ViewModels
+{
+    public delegate void AvailabilityHandler(bool available);
+    public class CampingMapViewModel() : INotifyPropertyChanged
+    {
+        public event AvailabilityHandler? AvailabilityChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private ObservableCollection<CampingSpotVisualModel> campingSpots = [];
+        public ObservableCollection<CampingSpotVisualModel> CampingSpots
+        {
+            get => campingSpots;
+            set
+            {
+                campingSpots = value;
+                OnPropertyChanged(nameof(CampingSpots));
+            }
+        }
+
+        private string backgroundImage = "";
+        public string BackgroundImage
+        {
+            get => backgroundImage;
+            set
+            {
+                backgroundImage = value;
+                OnPropertyChanged(nameof(BackgroundImage));
+            }
+        }
+
+        public CampingMapViewModel(string backgroundImage, List<CampingSpot> campingSpots) : this()
+        {
+            var campingSpotVisuals = new CampingSpotVisualModel[campingSpots.Count];
+            for (int i = 0; i < campingSpots.Count; i++)
+            {
+                CampingSpot spot = campingSpots[i];
+                campingSpotVisuals[i] = new(spot.ID, spot.PositionX, spot.PositionY);
+            }
+
+            this.campingSpots = new(campingSpotVisuals);
+            this.backgroundImage = backgroundImage;
+        }
+
+        public void ClearAvailability()
+        {
+            SetAvailability([]);
+        }
+
+        public void SetAvailability(Dictionary<int, CampingSpot> available)
+        {
+            if (available.Count == 0)
+            {
+                AvailabilityChanged?.Invoke(false);
+            }
+
+            foreach (var visual in campingSpots)
+            {
+                if (available.ContainsKey(visual.ID))
+                {
+                    visual.Available = true;
+                }
+                else
+                {
+                    visual.Available = false;
+                }
+            }
+
+            OnPropertyChanged(nameof(CampingSpots));
+            AvailabilityChanged?.Invoke(true);
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this.PropertyChanged, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ EmployeeApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CampingApplication.Business;
+using CampingApplication.Business.CampingSpotService;
+using CampingApplication.VisitorApp.Models;
+
+namespace CampingApplication.VisitorApp.ViewModels
+{
+    public class MainViewModel
+    {
+        public CampingSpotService CampingSpotService { get; private set; }
+        public CampingMapViewModel CampingMapViewModel { get; private set; }
+        private List<CampingSpot> campingSpots = [];
+
+        public MainViewModel()
+        {
+            CampingSpotService = ServiceProvider.Current.Resolve<CampingSpotService>();
+            campingSpots = CampingSpotService.GetCampingSpots();
+
+            CampingMapViewModel = new("../test1.png", campingSpots);
+        }
+
+        public void CheckAvailableSpots(DateTime startDate, DateTime endDate)
+        {
+            var availableSpots = CampingSpotService.GetAvailableSpots([.. campingSpots], startDate, endDate);
+
+            Dictionary<int, CampingSpot> availableDict = [];
+            foreach (var available in availableSpots)
+            {
+                availableDict.TryAdd(available.ID, available);
+            }
+
+            Debug.WriteLine($"{availableDict.Count} camping spots available");
+
+            CampingMapViewModel.SetAvailability(availableDict);
+        }
+    }
+}
